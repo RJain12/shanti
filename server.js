0b1310e26fb9257031afc9c2816688e5ec1f3efd
Rishab Jain <rishabjain@college.harvard.edu>
2023-10-21 19:24:54 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
+// server.js
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 3000;
 
-// Serve the React Native landing page
-app.use(express.static(path.join(__dirname, 'public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Start the Express server
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
 });
