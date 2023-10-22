a82b78e77ab0fe378bb86d2a153eb84a1822722b
RJain12 <rishabjain@college.harvard.edu>
2023-10-21 21:10:08 -0400

    Added unicorn world image

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ app.get('/viewer', (req, res) => {
   res.sendFile(path.join(__dirname, 'src', 'viewer.html'));
 });
 
+app.get('/viewer2', (req, res) => {
+  res.sendFile(path.join(__dirname, 'src', 'viewer2.html'));
+});
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'src', 'index.html'));
 });
